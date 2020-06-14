481b990136a2f1641d6f1f91b1d6b70ddc5d0de2
IsraPerez98 <isra1998isra@gmail.com>
2020-06-14 02:49:36 -0400

    bugfixes de los productos

--- src/componentes/pestanas/productos.js
+++ src/componentes/pestanas/productos.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class BloqueProducto extends Component {
+class BloqueProducto extends Component { // los bloques que componen cada producto
     render() {
         const id = this.props.id;
         const nombre = this.props.nombre;
@@ -34,15 +34,16 @@ class Productos extends Component {
 
         this.state = {
             cargado: false,
-            datos_productos: "",
+            error: null,
+            datos_productos: "", // aqui almacenamos los datos de los productos cuando se llama a la api
         };
 
         this.obtenerProductosAPI = this.obtenerProductosAPI.bind(this);
     }
 
-    async obtenerProductosAPI() { 
+    async obtenerProductosAPI() { //llamamos a la api y refrescamos los valores
 
-        const jwt_acceso = await this.props.ObtenerJWTAcceso();
+        const jwt_acceso = await this.props.ObtenerJWTAcceso(); 
 
         //console.log("jwt productos ", jwt_acceso);
         
@@ -55,17 +56,21 @@ class Productos extends Component {
             const productos = res.data;
             //const productos = JSON.parse(res.data);
             console.log(productos);
-            this.setState({
+            this.setState({ // seteamos el estado del componente con los datos de la api
                 cargado: true,
                 datos_productos: productos,
-            })
+            });
         })
         .catch( (error, res) => { // algun error
+            this.setState({
+                cargado: true,
+                error: JSON.stringify(error), // para que el error siempre se pase como texto
+            });
             console.log(error);
             console.log(error.response);
             if("response" in error ) {
                 if("data" in error.response){
-                    //alert(error.response.data);
+                    alert(error.response.data);
                 } 
             }else {
                 alert("Error Desconocido");
@@ -74,15 +79,17 @@ class Productos extends Component {
         } )
     }
 
-    componentDidMount() {
+    componentDidMount() { // cuando se monte el componenete llamamos a la api
         this.obtenerProductosAPI();
     }
 
     render() {
         const esta_logeado = this.props.estaLogeado();
         const cargado = this.state.cargado;
+        const error = this.state.error;
         const productos = this.state.datos_productos;
         
+        if(error) return <div> ERROR {error} </div>
         if(!esta_logeado) return <div> Debe ingresar con su cuenta para acceder a este panel. </div>;
         if(!cargado) return <div> Cargando... </div>;
 
