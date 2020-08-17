e10775a002a2c2afe1203600768cdaebb7c5a23f
IsraPerez98 <isra1998isra@gmail.com>
2020-08-16 20:06:26 -0400

    restructuracion codigo registros

--- src/componentes/pestanas/registros/registros.js
+++ src/componentes/pestanas/registros/registros.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import RegistrosPlegables from './registros_plegables/registros_plegables';
 
@@ -9,14 +10,131 @@ class Registro extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            registro_vende: {
+                cargado: false, // si se han cargado los registros de las ventas
+                error: null,
+                datos: {}, // donde almacenamos los registros de las ventas
+            },
+            registro_compra: {
+                cargado: false, // si se han cargado los registros de las compras
+                error: null,
+                datos: {}, // donde almacenamos los registros de las compras
+            }
+        }
+        this.obtenerRegistrosVentas = this.obtenerRegistrosVentas.bind(this);
+        this.obtenerRegistrosCompras = this.obtenerRegistrosCompras.bind(this);
+    }
+
+    async obtenerRegistrosVentas() {
+        const jwt_acceso = await this.props.ObtenerJWTAcceso(); 
+        
+        axios.get('/api/get/registrosventas', { // llamamos a la api pidiendo los registros de las ventas
+            headers: {
+                "Authorization" : `Bearer ${jwt_acceso}`,
+            }
+        })
+        .then( res => {
+            const registro_vende = res.data;
+            
+            //console.log(registro_vende);
+            this.setState({ // seteamos el estado del componente con los datos de la api
+                registro_vende: {
+                    cargado: true,
+                    datos: registro_vende,
+                },
+            });
+        })
+        .catch( (error, res) => { // algun error
+            this.setState({
+                registro_vende : {
+                    cargado: true,
+                    error: JSON.stringify(error), // para que el error siempre se pase como texto
+                },
+            });
+            console.log(error);
+            console.log(error.response);
+            if("response" in error ) {
+                if("data" in error.response){
+                    alert(error.response.data);
+                } 
+            }else {
+                alert("Error Desconocido");
+            }
+            
+        } )
+    }
+
+    async obtenerRegistrosCompras() {
+        const jwt_acceso = await this.props.ObtenerJWTAcceso(); 
+        
+        axios.get('/api/get/registroscompras', { // llamamos a la api pidiendo los registros de las compras
+            headers: {
+                "Authorization" : `Bearer ${jwt_acceso}`,
+            }
+        })
+        .then( res => {
+            const registro_compra = res.data;
+            
+            //console.log(registro_compra);
+            this.setState({ // seteamos el estado del componente con los datos de la api
+                registro_compra: {
+                    cargado: true,
+                    datos: registro_compra,
+                },
+            });
+        })
+        .catch( (error, res) => { // algun error
+            this.setState({
+                registro_vende : {
+                    cargado: true,
+                    error: JSON.stringify(error), // para que el error siempre se pase como texto
+                },
+            });
+            console.log(error);
+            console.log(error.response);
+            if("response" in error ) {
+                if("data" in error.response){
+                    alert(error.response.data);
+                } 
+            }else {
+                alert("Error Desconocido");
+            }
+            
+        } )
+    }
+
+    componentDidMount() {
+        //obtenemos los datos desde la API cuando el componente se monte
+        this.obtenerRegistrosVentas();
+        this.obtenerRegistrosCompras();
     }
 
     render() {
+        const cargado = this.state.registro_compra.cargado && this.state.registro_vende.cargado;
+        const error = this.state.registro_compra.error || this.state.registro_vende.error;
+        const registro_vende = this.state.registro_vende.datos;
+        const registro_compra = this.state.registro_compra.datos;
+ 
+        if(error) return <div> ERROR: {error} </div>;
+        if(!cargado) return <div> Cargando .... </div>;
+
+
         return(
             <div className="pestaña-registros">
+                <div className="registros-dinero">
+                   <div className="cuadro-dinero-mes">
+
+                    </div> 
+                    <div className="cuadro-dinero-año">
+
+                    </div>
+                </div>
                 <div className="registros-plegables">
                     <RegistrosPlegables
-                        ObtenerJWTAcceso={this.props.ObtenerJWTAcceso}
+                        registro_compra={registro_compra}
+                        registro_vende={registro_vende}
                     />
                 </div>
             </div>
--- src/componentes/pestanas/registros/registros_plegables/registros_plegables.js
+++ src/componentes/pestanas/registros/registros_plegables/registros_plegables.js
@@ -1,115 +1,16 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
 import RegistroVentaPlegable from './registro_venta_plegable';
 import RegistroCompraPlegable from './registro_compra_plegable';
 
 class RegistrosPlegables extends Component {
-    // se le pasa this.props.ObtenerJWTAcceso()
+    //se le pasa this.props.registro_vende
+    // y this.props.registro_compra
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            registro_vende: {
-                cargado: false, // si se han cargado los registros de las ventas
-                error: null,
-                datos: {}, // donde almacenamos los registros de las ventas
-            },
-            registro_compra: {
-                cargado: false, // si se han cargado los registros de las compras
-                error: null,
-                datos: {}, // donde almacenamos los registros de las compras
-            }
-        }
-
-        this.obtenerRegistrosVentas = this.obtenerRegistrosVentas.bind(this);
-        this.obtenerRegistrosCompras = this.obtenerRegistrosCompras.bind(this);
         this.compararFechas = this.compararFechas.bind(this);
-    }
-
-    async obtenerRegistrosVentas() {
-        const jwt_acceso = await this.props.ObtenerJWTAcceso(); 
-        
-        axios.get('/api/get/registrosventas', { // llamamos a la api pidiendo los registros de las ventas
-            headers: {
-                "Authorization" : `Bearer ${jwt_acceso}`,
-            }
-        })
-        .then( res => {
-            const registro_vende = res.data;
-            
-            //console.log(registro_vende);
-            this.setState({ // seteamos el estado del componente con los datos de la api
-                registro_vende: {
-                    cargado: true,
-                    datos: registro_vende,
-                },
-            });
-        })
-        .catch( (error, res) => { // algun error
-            this.setState({
-                registro_vende : {
-                    cargado: true,
-                    error: JSON.stringify(error), // para que el error siempre se pase como texto
-                },
-            });
-            console.log(error);
-            console.log(error.response);
-            if("response" in error ) {
-                if("data" in error.response){
-                    alert(error.response.data);
-                } 
-            }else {
-                alert("Error Desconocido");
-            }
-            
-        } )
-    }
-
-    async obtenerRegistrosCompras() {
-        const jwt_acceso = await this.props.ObtenerJWTAcceso(); 
-        
-        axios.get('/api/get/registroscompras', { // llamamos a la api pidiendo los registros de las compras
-            headers: {
-                "Authorization" : `Bearer ${jwt_acceso}`,
-            }
-        })
-        .then( res => {
-            const registro_compra = res.data;
-            
-            //console.log(registro_compra);
-            this.setState({ // seteamos el estado del componente con los datos de la api
-                registro_compra: {
-                    cargado: true,
-                    datos: registro_compra,
-                },
-            });
-        })
-        .catch( (error, res) => { // algun error
-            this.setState({
-                registro_vende : {
-                    cargado: true,
-                    error: JSON.stringify(error), // para que el error siempre se pase como texto
-                },
-            });
-            console.log(error);
-            console.log(error.response);
-            if("response" in error ) {
-                if("data" in error.response){
-                    alert(error.response.data);
-                } 
-            }else {
-                alert("Error Desconocido");
-            }
-            
-        } )
-    }
-
-    componentDidMount() {
-        //obtenemos los datos desde la API cuando el componente se monte
-        this.obtenerRegistrosVentas();
-        this.obtenerRegistrosCompras();
     }
 
     compararFechas(a,b) { // para ordenar los registros por fechas
@@ -128,13 +29,9 @@ class RegistrosPlegables extends Component {
     }
 
     render() {
-        const cargado = this.state.registro_compra.cargado && this.state.registro_vende.cargado;
-        const error = this.state.registro_compra.error || this.state.registro_vende.error;
-        const registro_vende = this.state.registro_vende.datos;
-        const registro_compra = this.state.registro_compra.datos;
- 
-        if(error) return <div> ERROR: {error} </div>;
-        if(!cargado) return <div> Cargando .... </div>;
+        const registro_vende = this.props.registro_vende;
+        const registro_compra = this.props.registro_compra;
+
 
         // en este arreglo tendremos los registros de las ventas y las compras juntos
         let registros_combinados = registro_vende.concat(registro_compra);
